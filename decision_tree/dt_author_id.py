fcb686295008f912d9efa2ffe0d9786d58928e52
evykassirer <evy.kassirer@gmail.com>
2015-12-21 11:59:44 -0500

    unit 3

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -20,11 +20,13 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
 
+print clf.score(features_test, labels_test)
 
 #########################################################
 
