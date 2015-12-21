9df8badd08a7b13d2d5fb94d3cdc4eb6e0a37d62
evykassirer <evy.kassirer@gmail.com>
2015-12-20 19:32:34 -0500

    unit 2

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -17,14 +17,24 @@ from email_preprocess import preprocess
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
-
-
-
+features_train, features_test, labels_train, labels_test = preprocess() 
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
 
 #########################################################
 ### your code goes here ###
 
 #########################################################
+from sklearn import svm
+clf = svm.SVC(kernel='rbf', C=10000)
+clf.fit(features_train, labels_train)
 
+#print(clf.score(features_test, labels_test))
 
+predictions = clf.predict(features_test)
+count = 0
+for p in predictions:
+	if p == 1:
+		count += 1
+
+print count
