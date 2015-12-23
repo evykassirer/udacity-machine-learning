887c04d10a017b22a9202ff80b38ce603a963651
evykassirer <evy.kassirer@gmail.com>
2015-12-22 22:24:08 -0500

    leson 7

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -11,10 +11,20 @@ def outlierCleaner(predictions, ages, net_worths):
         each tuple is of the form (age, net_worth, error).
     """
     
-    cleaned_data = []
+    data = []
+    size = len(predictions)
+    for i in range(size):
+        error = (predictions[i][0] - net_worths[i][0]) * (predictions[i][0] - net_worths[i][0])
+        data.append((ages[i][0],
+                     net_worths[i][0],
+                     error))
 
+
+    data.sort(key=lambda tup: tup[2])
     ### your code goes here
+#    print data
 
-    
+    cleaned_data = data[:81]
+
     return cleaned_data
 
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -27,8 +27,11 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### the plotting code below works, and you can see what your regression looks like
 
 
-
-
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(ages_train, net_worths_train)
+print reg.coef_
+print reg.score(ages_test, net_worths_test)
 
 
 
@@ -68,6 +71,7 @@ if len(cleaned_data) > 0:
     ### refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
+        print reg.score(ages_test, net_worths_test)
         plt.plot(ages, reg.predict(ages), color="blue")
     except NameError:
         print "you don't seem to have regression imported/created,"
@@ -77,7 +81,6 @@ if len(cleaned_data) > 0:
     plt.xlabel("ages")
     plt.ylabel("net worths")
     plt.show()
-
 
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
